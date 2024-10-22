1014beeb1f9e629c2deb72fb0a249aa92cff7323
ilaharustamli <ilahasr-ap118@code.edu.az>
2024-10-22 13:46:22 +0400

    Abstract task

---
+++ TaskTodayAbstrackt/TaskToday/Models/SalariedEmployee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskToday.Models
+{
+    internal class SalariedEmployee : Employee
+    {
+        public double MonthlySalary { get; set; }
+
+        public SalariedEmployee(string name, string surname, int age, double monthlySalary) : base(name, surname, age)
+        { 
+            MonthlySalary = monthlySalary;
+        }
+
+        protected override double CalculateSalary()
+        {
+            return MonthlySalary;
+        }
+
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($"ID: {Id} \nName: {Name} \nSurname: {Surname}\nAge: {Age} \nMonthly Salary: {CalculateSalary}");
+        }
+    }
+}
